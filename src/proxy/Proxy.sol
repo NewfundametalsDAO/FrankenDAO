1fae6aa1d0c7ddd7c82fc411bcd9fa6067b493ec
Zach Obront <zobront@gmail.com>
2022-10-07 11:01:30 -0500

    proxy exeternal fxs

--- src/proxy/Proxy.sol
+++ src/proxy/Proxy.sol
@@ -5,4 +5,21 @@ import "oz/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract FrankenDAOProxy is ERC1967Proxy {
     constructor(address _logic, bytes memory _data) ERC1967Proxy(_logic, _data) {}
+
+    modifier onlyProxyAdmin() {
+        require(msg.sender == _getAdmin(), "Proxy: caller is not the proxy admin");
+        _;
+    }
+
+    function setProxyAdmin(address newAdmin) external onlyProxyAdmin {
+        _changeAdmin(newAdmin);
+    }
+
+    function upgradeImplementation(address newImplementation) external onlyProxyAdmin {
+        _upgradeTo(newImplementation);
+    }
+
+    function upgradeImplementationAndCall(address newImplementation, bytes calldata data) external payable onlyProxyAdmin {
+        _upgradeToAndCall(newImplementation, data, false);
+    }
 }
