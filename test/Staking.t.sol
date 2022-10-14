0d5003f724e91a419934236784ca71bb98fbcea1
Zach Obront <zobront@gmail.com>
2022-10-14 11:26:47 -0500

    evil score test

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Staking.sol";
+import "oz/utils/Strings.sol";
 
 contract StakingTest is Test {
     Staking staking;
@@ -24,13 +25,20 @@ contract StakingTest is Test {
     // Test only exists for sample data, where all evens are evil. Redo with JSON call once we have final.
     function testEvilScores(uint tokenId) public {
         vm.assume(tokenId < 20000);
-        if (tokenId % 2 == 1 || tokenId >= 10000) {
+        if (tokenId > 9999) {
             assert(staking.evilBonus(tokenId) == 0);
         } else {
-            assert(staking.evilBonus(tokenId) == 10);
-        }
-        
+            string memory json = vm.readFile("static/evilScores.json");
+            string memory tokenLookup = string(abi.encodePacked(".", Strings.toString(tokenId)));
+            uint correct = abi.decode(vm.parseJson(json, tokenLookup), (uint)) * 10;
+            uint bonus = staking.evilBonus(tokenId);
+            assert(bonus == correct);
+        }        
     }
+
+    // function testGetEvilScores() public {
+    //     console.log(staking.evilBonus(2158));
+    // }
 
     // make sure transferFrom blocks all token transfers (safe with both sigs)
     // test evil bonus
