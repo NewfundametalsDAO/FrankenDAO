a58eefc9db900d76f7d389a6b3d3ab952edab663
Zakk <zakk@hawthorne.io>
2022-10-25 11:53:32 -0500

    he

--- test/staking/Locked.t.sol
+++ test/staking/Locked.t.sol
@@ -4,16 +4,10 @@ import "forge-std/Test.sol";
 import "../../src/Staking.sol";
 import "../utils/mocks/Token.sol";
 
-import { StakingBase } from "./StakingBase.t.sol";
+import {GovernanceBase} from "../governance/GovernanceBase.sol";
 
-contract LockedTest is StakingBase {
+contract LockedTest is GovernanceBase {
     uint[] ids = [6251, 4122];
-    address[] targets = [0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B,0x35A18000230DA775CAc24873d00Ff85BccdeD550,0x70e36f6BF80a52b3B46b3aF8e106CC0ed743E8e4];
-    uint256[] values = [0,0,0];
-    string[] signatures
-    = ["_setMarketBorrowCaps(address[],uint256[])","_setInterestRateModel(address)","_setInterestRateModel(address)"];
-    bytes[] data = "0x00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000100000000000000000000000070e36f6bf80a52b3b46b3af8e106cc0ed743e8e40000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000001fc3842bd1f071c00000,0x000000000000000000000000d956188795ca6f4a74092ddca33e0ea4ca3a1395,0x000000000000000000000000d88b94128ff2b8cf2d7886cd1c1e46757418ca2a";
-
 
     // reverts if unstaking during a vote
     function testLocking__RevertsIfUnstakingDuringVoting() public {
@@ -27,12 +21,6 @@ contract LockedTest is StakingBase {
         frankenpunks.delegate(playerOne);
 
         // create proposal
-        uint256 proposalId = govImpl.propose(
-            targets,
-            values,
-            signatures,
-            data
-        );
 
         // addr 1 votes
         vm.prank(playerOne);
