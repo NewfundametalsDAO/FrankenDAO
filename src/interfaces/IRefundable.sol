ba40a558e4998b4c07ad70b1ffa5ce2d18f84cd5
Zakk <zakk@hawthorne.io>
2022-11-01 13:14:35 -0500

    update refundable interface

--- src/interfaces/IRefundable.sol
+++ src/interfaces/IRefundable.sol
@@ -1,10 +1,25 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.10;
 
-abstract contract IRefundable {
-    /// @notice Error thrown when the contract balance is too low to refund gas
-    error InsufficientRefundBalance();
+interface IRefundable {
+
+    ////////////////////
+    ////// Events //////
+    ////////////////////
 
     /// @notice Emitted when a refund is issued
     event IssueRefund(address refunded, uint256 amount, bool sent);
+
+    ////////////////////
+    ////// Errors //////
+    ////////////////////
+
+    /// @notice Error thrown when the contract balance is too low to refund gas
+    error InsufficientRefundBalance();
+
+    /////////////////////
+    ////// Methods //////
+    /////////////////////
+
+    function MAX_REFUND_PRIORITY_FEE() external view returns (uint256);
+    function REFUND_BASE_GAS() external view returns (uint256);
 }
